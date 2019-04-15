c75cce18564b66367f78aabfd36360e38ad5914d
massya92 <mrebindaine@hotmail.com>
2019-04-15 15:34:58 +0200

    fichiers inutiles supprimes

--- demo_factory/car_citadine.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class CarCitadine extends Car
-{
-
-}
--- demo_factory/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('CarFactory.php');
-require('car.php');
-require('car_citadine.php');
-require('car_4x4.php');
-$car= CarFactory::getCar('4x4');
-var_dump($car);
